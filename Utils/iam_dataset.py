d7f2a0163cf63bdb8a24a22d0ac7d360494d111b
teja00 <tejanagubandi00@gmail.com>
2024-07-03 20:15:51 -0500

    iam dataset change

--- Utils/iam_dataset.py
+++ Utils/iam_dataset.py
@@ -1,14 +1,11 @@
 import sys
 import os
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, project_root)
-
+import torch
 import torchvision
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
 from torch.utils.data.dataset import Dataset
-
 from Utils.pre_trained_utils import get_image_model_processor, get_image_representation
 
 class IAMDataset(Dataset):
@@ -18,7 +15,7 @@ class IAMDataset(Dataset):
     are under one directory.
     """
     
-    def __init__(self, split, im_path, device, im_size=256, im_channels=3, im_ext='png',condition_config=None):
+    def __init__(self, split, im_path, device, im_size=256, im_channels=3, im_ext='png', condition_config=None):
         self.split = split
         self.im_size = im_size
         self.im_channels = im_channels
@@ -28,7 +25,6 @@ class IAMDataset(Dataset):
         
         self.condition_types = [] if condition_config is None else condition_config['condition_types']
         
-            
         self.images, self.texts = self.load_images(im_path)
         
     
@@ -43,10 +39,9 @@ class IAMDataset(Dataset):
         file_content = file.readlines()
         texts = []
         
-        
-        for content in (file_content):
+        for content in file_content:
             split_content = content.split(' ')
-            if('#' in split_content[0]):
+            if '#' in split_content[0]:
                 continue
 
             if 'text' in self.condition_types:
@@ -58,8 +53,10 @@ class IAMDataset(Dataset):
                 image_name_folder_2 = image_name_folder_1 + '-' + image_name.split('-')[1] 
                 image_final_folder = image_name_folder_1 + '/' + image_name_folder_2 + '/' + image_name
                 ims.append(os.path.join(im_path, 'words/{}.{}'.format(image_final_folder, self.im_ext)))
+                
         if 'text' in self.condition_types:
             assert len(texts) == len(ims), "Condition Type Text but could not find captions for all images"
+        
         print('Found {} images'.format(len(ims)))
         print('Found {} captions'.format(len(texts)))
         return ims, texts
@@ -68,36 +65,31 @@ class IAMDataset(Dataset):
         return len(self.images)
     
     def __getitem__(self, index):
-    ######## Set Conditioning Info ########
+        ######## Set Conditioning Info ########
         cond_inputs = {}
         if 'text' in self.condition_types:
             cond_inputs['text'] = self.texts[index]
 
         #######################################
         
-        im = Image.open(self.images[index])
-        im = im.to(self.device).detach()
+        im = Image.open(self.images[index]).convert('RGB')
         transform = torchvision.transforms.Compose([
             torchvision.transforms.Resize(self.im_size),
             torchvision.transforms.CenterCrop(self.im_size),
             torchvision.transforms.ToTensor(),
         ])
         
-        im_tensor = transform(im)
+        im_tensor = transform(im).to(self.device)
 
         # Convert input to -1 to 1 range.
         im_tensor = (2 * im_tensor) - 1
 
         # conditional image tensor below
-
-        image_model, image_processor = get_image_model_processor(self.device)
-        
-
         if 'image' in self.condition_types:
-            cond_inputs['image'] = get_image_representation(im,image_model=image_model, image_processor=image_processor,device=self.device)
+            image_model, image_processor = get_image_model_processor(self.device)
+            cond_inputs['image'] = get_image_representation(im_tensor.unsqueeze(0), image_model=image_model, image_processor=image_processor, device=self.device).squeeze(0).detach()
 
         im.close()
-
 
         if len(self.condition_types) == 0:
             return im_tensor
