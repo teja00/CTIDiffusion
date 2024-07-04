1817449aea246ac79b08106d4166ce7c7f1834b9
teja00 <tejanagubandi00@gmail.com>
2024-07-03 20:09:38 -0500

    fixing multiple spawn issue

--- Training/train.py
+++ Training/train.py
@@ -3,12 +3,9 @@ import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
-from wsgiref import validate
 import torch
-from PIL import Image
 import yaml
 import argparse
-import os
 import numpy as np
 from tqdm import tqdm
 from torch.optim import Adam
@@ -18,22 +15,19 @@ from torch.cuda.amp import autocast, GradScaler
 from Utils.iam_dataset import IAMDataset
 from Utils.config_utils import get_config_value, validate_image_config, validate_text_config
 from Utils.diffusion_utils import drop_image_condition, drop_text_condition
-from Utils.pre_trained_utils import get_text_representation, get_tokenizer_and_model, get_image_model_processor, get_image_representation
+from Utils.pre_trained_utils import get_text_representation, get_tokenizer_and_model, get_image_model_processor
 from Model.Unet import Unet
 from Model.NoiseScheduler import NoiseScheduler
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-
-# TODO Style component for training we need to do
 
 def train(args):
     # Read the config file #
     torch.cuda.empty_cache()
     
     # Set PYTORCH_CUDA_ALLOC_CONF
-    import os
     os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128'
+    
     with open(args.config_path, 'r') as file:
         try:
             config = yaml.safe_load(file)
@@ -50,30 +44,27 @@ def train(args):
     
     # Create the noise scheduler
     scheduler = NoiseScheduler(num_timesteps=diffusion_config['num_timesteps'],
-                                     beta_start=diffusion_config['beta_start'],
-                                     beta_end=diffusion_config['beta_end'])
+                               beta_start=diffusion_config['beta_start'],
+                               beta_end=diffusion_config['beta_end'])
     
     text_tokenizer = None
     text_model = None
     empty_text_embed = None
-    # emtpy_style_embed = None
     empty_image_embed = None
     condition_types = []
     condition_config = get_config_value(diffusion_model_config, key='condition_config', default_value=None)
     if condition_config is not None:
         assert 'condition_types' in condition_config, "condition_types missing in condition_config"
         condition_types = condition_config['condition_types']
-        if ('text') in condition_types:
+        if 'text' in condition_types:
             validate_text_config(condition_config)
-            # TODO: we need to create a text model as below
             text_tokenizer, text_model = get_tokenizer_and_model(condition_config['text_condition_config']['text_embed_model'], device=device)
             empty_text_embed = get_text_representation([''], text_tokenizer, text_model, device)
         if 'image' in condition_types:
             validate_image_config(condition_config)
             empty_image_embed = torch.zeros((3, dataset_config['im_size'], dataset_config['im_size']), device=device)
 
-
-    # TODO: we need to create a dataset Class as below
+    # Create the dataset
     im_dataset_cls = {
         'IAMHandwriting': IAMDataset,
     }.get(dataset_config['name'])
@@ -83,12 +74,13 @@ def train(args):
                                 im_size=dataset_config['im_size'],
                                 im_channels=dataset_config['im_channels'],
                                 condition_config=condition_config,
-                                device = device)
+                                device=device)
     
     data_loader = DataLoader(im_dataset,
                              batch_size=train_config['ldm_batch_size'],
                              shuffle=True,
-                             num_workers= train_config['num_workers'])
+                             num_workers=train_config['num_workers'],
+                             pin_memory=True)  # Ensure memory pinning for performance
     
     # Instantiate the model
     model = Unet(im_channels=autoencoder_model_config['z_channels'],
@@ -100,14 +92,12 @@ def train(args):
     optimizer = Adam(model.parameters(), lr=train_config['ldm_lr'])
     criterion = torch.nn.MSELoss()
     
-    # mixed precision setup
+    # Mixed precision setup
     scaler = GradScaler()
 
     for epoch_idx in range(num_epochs):
         losses = []
         for data in tqdm(data_loader):
-            # TODO We need to check when the data is coming we need to check the image conditioning data
-            # did not do any work
             optimizer.zero_grad()
             cond_input = None
             if condition_config is not None:
@@ -129,8 +119,6 @@ def train(args):
                     assert 'image' in cond_input, "Image condition missing in cond_input"
                     validate_image_config(condition_config=condition_config)
                     image_condition = cond_input['image'].to(device)
-                    # image_drop_prob = get_config_value(condition_config['image_condition_config'], 'cond_drop_prob', 0.0)
-                    # image_condition = drop_image_condition(image_condition, empty_image_embed, image_drop_prob)
                     cond_input['image'] = image_condition
                         
             # Sample random noise
@@ -159,6 +147,9 @@ def train(args):
     print('Done Training ...')
 
 if __name__ == '__main__':
+    import torch.multiprocessing as mp
+    mp.set_start_method('spawn', force=True)  # Use 'spawn' method for starting processes
+
     parser = argparse.ArgumentParser(description='Arguments for ddpm training')
     parser.add_argument('--config', dest='config_path',
                         default='config/IAM.yaml', type=str)
